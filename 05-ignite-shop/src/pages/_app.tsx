25f8b415d5d70641702208db8308dcd07c86fc2a
ipdmartins <ipdmartins@gmail.com>
2022-11-14 09:22:01 -0500

    nextjs concepts

---
+++ 05-ignite-shop/src/pages/_app.tsx
@@ -0,0 +1,13 @@
+import type { AppProps } from "next/app";
+import { globalStyles } from "../styles/global";
+
+globalStyles();
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <div>
+      <header></header>
+      <Component {...pageProps} />
+    </div>
+  );
+}
